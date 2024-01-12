cafae798fa5c7a14110dad61ca56b7162c99f36d
hoshiroo <108223413+hoshiroo1804@users.noreply.github.com>
2024-01-12 16:52:23 +0700

    edan

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import Upload from './component/upload';
+import Hasil from './component/hasil';
 
 const App = () => {
   return (
-    <div>
-      <Upload />
-    </div>
+    <Router>
+      <Routes>
+        <Route>
+        <Route path="/hasil" element={<Hasil/>}/>
+          <Route path="/upload" element={<Upload/>}/>
+        </Route>
+      </Routes>
+    </Router>
   );
 };
 
--- src/component/upload.jsx
+++ src/component/upload.jsx
@@ -1,26 +1,30 @@
 import React, { useState } from 'react';
+import { Spinner } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './upload.css';
 import { MdFavoriteBorder } from 'react-icons/md';
 import { MdOutlineAccountCircle } from 'react-icons/md';
-import { FiUploadCloud } from "react-icons/fi";
+import { useNavigate } from 'react-router-dom'; // Updated import
+import { Link } from 'react-router-dom';
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import { MDBFooter, MDBContainer, MDBCol, MDBRow, MDBIcon, MDBBtn} from 'mdb-react-ui-kit';
 
 const Upload = () => {
   const [selectedImage, setSelectedImage] = useState(null);
-  const [showAlert, setShowAlert] = useState(false);
+  const [showSpinner, setShowSpinner] = useState(false);
+  const navigate = useNavigate(); // Updated usage
 
   const handlePublish = () => {
-    // Logika atau aksi yang ingin dilakukan saat tombol ditekan
     console.log("Publish button clicked!");
-    // Tampilkan alert
-    setShowAlert(true);
+    setShowSpinner(true);
 
-    // Anda dapat menambahkan logika lain atau perubahan state di sini
-  };
-
-  const handleCloseAlert = () => {
-    // Tutup alert
-    setShowAlert(false);
+    // Simulate data processing
+    setTimeout(() => {
+      console.log('Data processing completed.');
+      setShowSpinner(false);
+      navigate('/hasil'); // Updated navigation
+    }, 2000);
   };
 
   const handleFileChange = (event) => {
@@ -29,17 +33,12 @@ const Upload = () => {
     if (file) {
       const reader = new FileReader();
       reader.onload = () => {
-        console.log('reader.result:', reader.result); // Tambahkan ini
+        console.log('reader.result:', reader.result);
         setSelectedImage(reader.result);
       };
       reader.readAsDataURL(file);
     }
   };
-  
-  const handleCloseImage = () => {
-    setSelectedImage(null);
-  };
-
 
   return (
     <div>
@@ -60,27 +59,28 @@ const Upload = () => {
             >
               <span className="navbar-toggler-icon"></span>
             </button>
+
             <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
               <ul className="navbar-nav">
                 <li className="nav-item">
-                  <a className="nav-link" href="#" style={{ color: 'white', fontSize: '18px' }}>
+                  <Link to="/hasil" className="nav-link" style={{ color: 'white', fontSize: '18px' }}>
                     Home
-                  </a>
+                  </Link>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="#" style={{ color: 'white', fontSize: '18px' }}>
+                  <Link to="/upload" className="nav-link" style={{ color: 'white', fontSize: '18px' }}>
                     Publish
-                  </a>
+                  </Link>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="#" style={{ color: 'white' }}>
+                  <Link to="/hasil" className="nav-link" style={{ color: 'white' }}>
                     <MdFavoriteBorder size={26} />
-                  </a>
+                  </Link>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="#" style={{ color: 'white' }}>
+                  <Link to="/upload" className="nav-link" style={{ color: 'white' }}>
                     <MdOutlineAccountCircle size={27} />
-                  </a>
+                  </Link>
                 </li>
               </ul>
             </div>
@@ -91,29 +91,29 @@ const Upload = () => {
       <section className="body-section">
         <div className="container">
           <h2>Body Section</h2>
-            <div className="box">
+          <div className="box">
             {/* Tampilkan gambar di dalam box jika ada yang dipilih */}
             {selectedImage && (
-                <>
+              <>
                 <img
-                    src={selectedImage}
-                    alt="Selected"
-                    className="selected-image"
-                    style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+                  src={selectedImage}
+                  alt="Selected"
+                  className="selected-image"
+                  style={{ width: '100%', height: '100%', objectFit: 'cover' }}
                 />
-                </>
+              </>
             )}
             {/* Input file tetap ada tetapi tidak ditampilkan */}
             <input
-                type="file"
-                id="myFile"
-                name="filename"
-                accept="image/*" // Hanya menerima file gambar
-                className="file-upload-input"
-                onChange={handleFileChange}
-                style={{ display: 'block', marginTop:'10px', marginLeft:'100px' }}
+              type="file"
+              id="myFile"
+              name="filename"
+              accept="image/*"
+              className="file-upload-input"
+              onChange={handleFileChange}
+              style={{ display: 'block', marginTop: '10px', marginLeft: '100px' }}
             />
-            </div>
+          </div>
 
           {/* Form 1 */}
           <div className="form">
@@ -139,36 +139,63 @@ const Upload = () => {
                 Terms and Conditions
               </a>
             </div>
-            <div className="paragraf">
-              <button
-                type="button"
-                className="button-upload"
-                style={{ color: 'black' }}
-                onClick={handlePublish}
-              >
-                Publish
-              </button>
+            <div className="paragraf" style={{ position: 'relative', minHeight: '200px' }}>
+              {/* Tampilkan Spinner jika showSpinner bernilai true */}
+              {showSpinner ? (
+                <div style={{ display: 'flex', justifyContent: 'center', position: 'absolute', top: 0, left: 0, right: 210, bottom: 0 }}>
+                  <Spinner animation="border" variant="primary" />
+                  <div>
+                  <p style={{color: 'rgba(0, 0, 0, 0.6)',marginLeft:'13px', marginTop:'5px'}}>Loading. . .</p>
+                  </div>
+                </div>
+              ) : (
+                <button
+                  type="button"
+                  className="button-upload"
+                  style={{ color: 'rgba(0, 0, 0, 0.6)' }}
+                  onClick={handlePublish}
+                >
+                  Publish
+                </button>
+              )}
             </div>
-            {/* Alert */}
-            {showAlert && (
-              <div className="custom-alert" onClick={handleCloseAlert}>
-                <img
-                  src="/src/icon/ceklist-removebg-preview.png"
-                  alt="Your Alt Text"
-                  style={{ width: '100%', height: 'auto' }}/>
-                <p style={{width:'100%', marginTop: '10px', marginLeft:'8px'}}>Your image is being processed</p>
-              </div>
-            )}
           </div>
         </div>
       </section>
 
       <footer className="footer-section">
-        {/* Konten dari bagian footer */}
-        <div className="container">
-          <h2>Footer Section</h2>
-          {/* Isi konten footer di sini */}
-        </div>
+            <MDBFooter className=' text-center text-white' style={{backgroundColor:'pink'}}>
+                <MDBContainer className='p-4 pb-0'>
+                    <section className='mb-4'>
+                    <MDBBtn outline color="light" floating className='m-1' href='#!' role='button'>
+                        <MDBIcon fab icon='facebook-f' />
+                    </MDBBtn>
+
+                    <MDBBtn outline color="light" floating className='m-1' href='#!' role='button'>
+                        <MDBIcon fab icon='twitter' />
+                    </MDBBtn>
+
+                    <MDBBtn outline color="light" floating className='m-1' href='#!' role='button'>
+                        <MDBIcon fab icon='google' />
+                    </MDBBtn>
+                    <MDBBtn outline color="light" floating className='m-1' href='#!' role='button'>
+                        <MDBIcon fab icon='instagram' />
+                    </MDBBtn>
+
+                    <MDBBtn outline color="light" floating className='m-1' href='#!' role='button'>
+                        <MDBIcon fab icon='linkedin-in' />
+                    </MDBBtn>
+
+                    <MDBBtn outline color="light" floating className='m-1' href='#!' role='button'>
+                        <MDBIcon fab icon='github' />
+                    </MDBBtn>
+                    </section>
+                </MDBContainer>
+
+                <div className='text-center p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
+                    © 2023 Copyright: Pawsitive
+                </div>
+                </MDBFooter>
       </footer>
     </div>
   );
